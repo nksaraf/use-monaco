3e9145dbb6d83766c5a89307ba2ca8412a6162f6
Nikhil Saraf <nsaraf98@gmail.com>
2020-05-28 16:05:45 -0400

    chnages

--- pkger.config.ts
+++ pkger.config.ts
@@ -3,6 +3,20 @@ export default {
   format: 'cjs,esm,umd',
   typecheck: true,
   entries: ['worker'],
+  // rollup: (config, options) => {
+  //   if (options.format === 'umd') {
+  //     console.log(config, options);
+  //     const oldExternal = config.external;
+  //     config.external = (label) => {
+  //       if (label === 'use-deep-compare-effect') {
+  //         return false;
+  //       }
+  //       // @ts-ignore
+  //       return oldExternal(...(arguments as any));
+  //     };
+  //   }
+  //   return config;
+  // },
   postBuild: (toolbox: Toolbox, config: any) => {
     toolbox.filesystem.copy(
       '.next/static/workers/prettier.monaco.worker.js',
