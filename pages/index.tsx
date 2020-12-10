a8db20fa710f9a8f25a20e9275d3bd70a190bf96
Nikhil Saraf <nsaraf98@gmail.com>
2020-12-10 15:56:43 -0800

    add worker loader config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,81 +1,35 @@
 import React from 'react';
-import {
-  useMonacoEditor,
-  typings,
-  prettier,
-  useEditor,
-  useTextModel,
-} from '../src';
+import { useMonacoEditor } from '../src';
 import themes from '../src/themes';
 
-const defaultValue = `<body>
-<div id="root"></div>
-<script defer type="module">
-  import {
-    useMonacoEditor,
-    prettier,
-  } from 'https://cdn.pika.dev/use-monaco@0.0.3';
-  import themes from 'https://cdn.pika.dev/use-monaco@0.0.3/themes';
-  import * as React from 'https://cdn.pika.dev/react';
-  import ReactDOM from 'https://cdn.pika.dev/react-dom';
-  import htm from 'https://cdn.pika.dev/htm';
-  const html = htm.bind(React.createElement);
+const defaultValue = `
+import {
+  useMonacoEditor,
+  prettier,
+} from 'https://cdn.pika.dev/use-monaco@0.0.3';
+import themes from 'https://cdn.pika.dev/use-monaco@0.0.3/themes';
+import * as React from 'https://cdn.pika.dev/react';
+import ReactDOM from 'https://cdn.pika.dev/react-dom';
+import htm from 'https://cdn.pika.dev/htm';
+const html = htm.bind(React.createElement);
 
-  let Editor = () => {
-    const { containerRef, monaco, model, loading } = useMonacoEditor({
-      plugins: [prettier(['graphq'])],
-      themes,
-      theme: 'github',
-      path: 'model.graphql',
-      defaultValue: ['type Query {}'].join('\\n'),
-    });
+let Editor = () => {
+  const { containerRef, monaco, model, loading } = useMonacoEditor({
+    plugins: [prettier(['graphq'])],
+    themes,
+    theme: 'github',
+    path: 'model.graphql',
+    defaultValue: ['type Query {}'].join('\n'),
+  });
 
-    return html\`<div
-      ref=\${containerRef}
-      style=\${{ height: 800, width: 600 }}
-    />\`;
-  };
+  return html\`<div
+    ref=\${containerRef}
+    style=\${{ height: 800, width: 600 }}
+  />\`;
+};
 
-  ReactDOM.render(html\`<\${Editor} />\`, document.getElementById('root'));
-</script>
-</body>
-`;
+ReactDOM.render(html\`<\${Editor} />\`, document.getElementById('root'));
 
-const otherDefaultValue = `<body>
-<div id="root"></div>
-<script defer type="module">
-  import {
-    useMonacoEditor,
-    prettier,
-  } from 'https://cdn.pika.dev/use-monaco@0.0.3';
-  import themes from 'https://cdn.pika.dev/use-monaco@0.0.3/themes';
-  import * as React from 'https://cdn.pika.dev/react';
-  import ReactDOM from 'https://cdn.pika.dev/react-dom';
-  import htm from 'https://cdn.pika.dev/htm';
-  const html = htm.bind(React.createElement);
-
-  let Editor = () => {
-    const { monaco, loading } = useMonaco({
-      plugins: [prettier(['graphql'])],
-      themes,
-      theme: 'github',
-    });
-
-    const model = useTextModel({
-      path: 'model.graphql',
-      defaultValue: ['type Query {}'].join('\n'),
-    });
-    const { containerRef } = useEditor({ monaco, model });
-
-    return html\`<div
-      ref=\${containerRef}
-      style=\${{ height: 800, width: 600 }}
-    />\`;
-  };
-
-  ReactDOM.render(html\`<\${Editor} />\`, document.getElementById('root'));
-</script>
-</body>
 `;
 
 let Editor = () => {
@@ -84,26 +38,15 @@ let Editor = () => {
       vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.20.0/min/vs',
     },
     themes: themes as any,
-    plugins: [prettier(['html']), typings()],
-    path: 'index.html',
-    language: 'html',
+    // plugins: [prettier(['html']), typings()],
+    path: 'index.ts',
+    language: 'typescript',
     defaultValue,
     theme: 'vs-light',
     // editorDidMount: (editor, monaco) => {
     //   monaco.languages.typescript.loadTypes('faunadb', '2.13.0');
     //   monaco.languages.typescript.exposeGlobal('faunadb', 'query', 'q');
     // },
-  });
-
-  const model = useTextModel({
-    defaultValue: otherDefaultValue,
-    path: 'index2.html',
-    monaco,
-  });
-
-  const { containerRef: editorRef } = useEditor({
-    monaco,
-    model,
   });
 
   return (
@@ -132,8 +75,7 @@ let Editor = () => {
         </a>
       </pre>
       <div style={{ display: 'flex', flex: 1 }}>
-        <div ref={containerRef} style={{ width: '50vw', height: '100%' }} />
-        <div ref={editorRef} style={{ width: '50vw', height: '100%' }} />
+        <div ref={containerRef} style={{ width: '100vw', height: '100%' }} />
       </div>
     </div>
   );
